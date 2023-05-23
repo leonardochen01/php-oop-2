70d8ccec901c00b7abe9271810940bdc7072e8c4
leonardochen01 <leonardochen.lc@gmail.com>
2023-05-23 17:25:52 +0200

    added: foreach cicle

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
   <title>Pet Store</title>
 </head>
 <body>
@@ -18,7 +19,7 @@
       require_once 'classes/CatProduct.php';
 
       //oggetti cani
-      $dogFood = new DogProduct('Cibo per cani', 'https://m.media-amazon.com/images/I/711K-07keuS._AC_SX522_.jpg', 14.99);
+      $dogFood = new DogProduct('Cibo per cani', 'https://i.ebayimg.com/images/g/hsoAAOSw~IBiTBdR/s-l500.jpg', 14.99);
       $dogToy = new DogProduct('Giocattolo per cani', 'https://staticfanpage.akamaized.net/wp-content/uploads/sites/5/2020/04/1.jpg', 8.99);
       $dogBed = new DogProduct('Cuccia per cani', 'https://m.media-amazon.com/images/I/71-tsDiw8iL._AC_SY355_.jpg', 39.99);
       $products[] = $dogFood;
@@ -32,6 +33,20 @@
       $products[] = $catFood;
       $products[] = $catToy;
       $products[] = $catBed;
+
+      //ciclo foreach delle carte contenente le informazioni dei prodotti
+      foreach ($products as $product) {
+        echo '<div class="col-4">'; 
+        echo '  <div class="card">'; 
+        echo '    <img src="' . $product->getImage() . '" alt="' . $product->getTitle() . '">';
+        echo '    <div class="card-body">';
+        echo '      <p class="card-text">Prezzo: €' . $product->getPrice() . '</p>';
+        echo '      <h5 class="card-title">' . $product->getTitle() . '</h5>';
+        echo '      <a href="#" class="btn btn-primary">Aggiungi al carrello</a>';
+        echo '    </div>'; //div card body
+        echo '  </div>';  //div card
+        echo '</div>'; //div colonna
+      }        
       ?>
     </div>
   </div>
